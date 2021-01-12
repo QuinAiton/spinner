0bb6f6f670d9929ca40acaffb2db48de79de7ca9
QuintenAiton <quinaiton@hotmail.com>
2021-01-12 19:38:50 +0000

    refactored spinner to make use of the setInterval function

---
+++ spinner2.js
@@ -0,0 +1,16 @@
+const spinner = () => {
+  let time = 100;
+  setInterval(() => {
+    if (time === 100) {
+      process.stdout.write('\r/   ');
+    } else if (time == 300) {
+      process.stdout.write('\r-   ');
+    } else if (time === 500) {
+      process.stdout.write('\r\\   ');
+    } else if (time === 700) {
+      process.stdout.write('\r|   ');
+    }
+    time += 200;
+  }, 700)
+}
+console.log(spinner())
